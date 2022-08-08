f527ad6728b91be46f2a7f2f01a560377e2db860
Chandra Sekhar Jampula <chandrajampula@gmail.com>
2022-08-08 10:27:24 +0100

    Script file Updated

--- csvconverter.py
+++ csvconverter.py
@@ -45,46 +45,50 @@ class JsonDataParser():
         return result
 
     def DataParser(self, datafilepth='Data/data1.csv', parsedfilepth='Results/parsed_json.json'):
-        with open(datafilepth) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter = ',')
-            for row in csv_reader:
-                if len(self.headers) == 0 :
-                    self.headers = row
-                    continue
-                baseNode = self.getBaseNode(row[0])
-                if baseNode is None or row[1] == "":
-                    continue
-                currentNode = None
-                for baseParent in baseNode.child:
-                    if baseParent.l_id == row[2]:
-                        currentNode = baseParent
-                        break
-                if currentNode is None:
-                    currentNode = LevelNode(row[1], row[2], row[3])
-                    baseNode.child.append(currentNode)
-                for x in range(0, len(row) - 4, 3):
-                    childNode = None
-                    if row[4+x+1] == "":
+        try:
+            with open(datafilepth) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter = ',')
+                for row in csv_reader:
+                    if len(self.headers) == 0 :
+                        self.headers = row
                         continue
-                    for child in currentNode.child:
-                        if child.l_id == row[4+x+1]:
-                            childNode = child
+                    baseNode = self.getBaseNode(row[0])
+                    if baseNode is None or row[1] == "":
+                        continue
+                    currentNode = None
+                    for baseParent in baseNode.child:
+                        if baseParent.l_id == row[2]:
+                            currentNode = baseParent
                             break
-                    if childNode is None:
-                        # childNode = LevelNode(row[4+x], row[4+x+1], row[4+x+2],row[4+x+3])
-                        childNode = LevelNode(row[4+x], row[4+x+1], row[4+x+2])
-                        currentNode.child.append(childNode)
-                    currentNode = childNode
-        #import pdb;pdb.set_trace()
-        result = []
-        for resultNode in self.totalNodes:
-            baseResult = {
-                self.headers[0]: resultNode.base_url,
-                "levels": self.loadChildLevel(resultNode.child, 1)
-            }
-            result.append(baseResult)
+                    if currentNode is None:
+                        currentNode = LevelNode(row[1], row[2], row[3])
+                        baseNode.child.append(currentNode)
+                    for x in range(0, len(row) - 4, 3):
+                        childNode = None
+                        if row[4+x+1] == "":
+                            continue
+                        for child in currentNode.child:
+                            if child.l_id == row[4+x+1]:
+                                childNode = child
+                                break
+                        if childNode is None:
+                            # childNode = LevelNode(row[4+x], row[4+x+1], row[4+x+2],row[4+x+3])
+                            childNode = LevelNode(row[4+x], row[4+x+1], row[4+x+2])
+                            currentNode.child.append(childNode)
+                        currentNode = childNode
+            #import pdb;pdb.set_trace()
+            result = []
+            for resultNode in self.totalNodes:
+                baseResult = {
+                    self.headers[0]: resultNode.base_url,
+                    "levels": self.loadChildLevel(resultNode.child, 1)
+                }
+                result.append(baseResult)
+        except Exception as e:
+            print(e)
+            result = []
 
-        json.dump(parsedfilepth,
+        json.dump(result,
                   open(parsedfilepth, 'w'),
                   indent=4,
                   sort_keys=False)
